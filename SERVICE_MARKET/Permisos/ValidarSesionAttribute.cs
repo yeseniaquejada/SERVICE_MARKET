cc6808a3c08af9ab9e9c79fbc9dbdf24c37cdf0b
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2022-09-20 10:25:11 -0500

    Cambios documentacion y modelos BD

--- SERVICE_MARKET/Permisos/ValidarSesionAttribute.cs
+++ SERVICE_MARKET/Permisos/ValidarSesionAttribute.cs
@@ -9,13 +9,13 @@ namespace SERVICE_MARKET.Permisos
     public class ValidarSesionAttribute : ActionFilterAttribute
     {
         /*METODO QUE PERMITE ENTRAR AL INDEX SOLO SI SE REGISTRO*/
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        /*public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             if (HttpContext.Current.Session["Usuario"] == null)
             {
                 filterContext.Result = new RedirectResult("~/Acceso/Login");
             }
             base.OnActionExecuting(filterContext);
-        }
+        }*/
     }
 }
