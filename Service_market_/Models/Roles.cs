73223c654ab4fa9dfc60a80df9cde374c754bec4
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2022-10-22 17:32:15 -0500

    proyecto

---
+++ Service_market_/Models/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Service_market_.Models
+{
+    public enum Roles
+    {
+        ADMINISTRADOR = 1,
+        PRESTADOR_SERVICIOS = 2,
+        CLIENTE = 3,
+    }
+}
