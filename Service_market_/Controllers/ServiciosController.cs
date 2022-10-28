a7eb7f587702ade5b2659bc0a36a8d9750f12fbd
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2022-10-28 06:41:05 -0500

    commit

--- Service_market_/Controllers/ServiciosController.cs
+++ Service_market_/Controllers/ServiciosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Service_market_.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,33 @@ namespace Service_market_.Controllers
     public class ServiciosController : Controller
     {
         // GET: Servicios
-        public ActionResult Index()
+        public ActionResult MisPublicaciones()
+        {
+            MantenimientoServicios ma = new MantenimientoServicios();
+            return View(ma.RecuperarTodos());
+        }
+
+        public ActionResult CrearPublicacion()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearPublicacion(FormCollection collection)
+        {
+            MantenimientoServicios ma = new MantenimientoServicios();
+            Servicios serv = new Servicios
+            {
+                NOMBRE_CAT = collection["NOMBRE_CAT"],
+                IMAGEN = collection["IMAGEN"],
+                NOMBRE = collection["NOMBRE"],
+                PRECIO = Decimal.Parse(collection["PRECIO"].ToString()),
+                TERMINOS = collection["TERMINOS"],
+                ID_CATEGORIA_FK = int.Parse(collection["ID_CATEGORIA_FK"].ToString())
+            };
+
+            ma.Publicacion(serv);
+            return RedirectToAction("MisPublicaciones");
         }
     }
 }
